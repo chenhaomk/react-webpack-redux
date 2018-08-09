9eb405a845fd5382e5319fe089a5e16f97e23e10
“e <ch@yingougou.com>
2018-08-09 22:22:12 +0800

    新增按需加载，优化缓存等

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import Home from 'pages/Home/home';
-import Page1 from 'pages/Page1/page1';
-import Counter from 'pages/Counter/counter';
+import Bundle from './bundle';
+
+import Home from 'bundle-loader?lazy&name=home!pages/Home/home';
+import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/page1';
+import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/counter';
+import UserInfo from 'bundle-loader?lazy&name=userInfo!pages/UserInfo/userInfo';
+
+const Loading = function () {
+    return <div>Loading...</div>
+};
+
+const createComponent = (component) => (props) => (
+    <Bundle load={component}>
+        {
+            (Component) => Component ? <Component {...props} /> : <Loading />
+        }
+    </Bundle>
+);
+
 
 const getRouter = () => (
     <Router>
@@ -13,11 +29,13 @@ const getRouter = () => (
                 <li><Link to="/">首页</Link></li>
                 <li><Link to="/page1">Page1</Link></li>
                 <li><Link to="/counter">Counter</Link></li>
+                <li><Link to="/userinfo">UserInfo</Link></li>
             </ul>
             <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/page1" component={Page1}/>
-                <Route path="/counter" component={Counter}/>
+                <Route exact path="/" component={createComponent(Home)} />
+                <Route path="/page1" component={createComponent(Page1)} />
+                <Route path="/counter" component={createComponent(Counter)} />
+                <Route path="/userinfo" component={createComponent(UserInfo)} />
             </Switch>
         </div>
     </Router>
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   name: "development",
@@ -11,9 +12,12 @@ const config = {
   ],
   //出口文件
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    //[name]是webpack的占位符，确保文件的唯一名称
-    filename: "[name].js"
+    // path: path.resolve(__dirname, 'dist'),
+    // //[name]是webpack的占位符，确保文件的唯一名称
+    // filename: "[name].js"
+    path: path.join(__dirname, './dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[chunkhash].js'
   },
   module: {
     rules: [{
@@ -40,7 +44,12 @@ const config = {
     contentBase: path.join(__dirname, './dist'),
     historyApiFallback: true,
     host: '0.0.0.0'
-  }
+  },
+  devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: path.join(__dirname, 'src/index.html')
+  })],
 }
 
 module.exports = config
